07a3b58ddb3d48d75d03aff1f6bd196bbd0dc771
Maxim <drachyov.maxim@gmail.com>
2014-08-28 21:58:03 +0400

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,13 +10,21 @@ titles = ['Sun', 'Moon', 'Storm', 'Scream', 'Howl', 'Life and Death', 'Jupiter',
 
 Artist.find_or_create_by_name('Von Goh')
 
+def fixtures_path
+  Rails.root.join("db/fixtures/")
+end
+
+def image
+  File.open Dir["#{fixtures_path}*.jpg"].sample, 'r'
+end
+
 75.times do
 	puts Artwork.create(
 		title: titles.sample,
-		description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore',
+		description: 'Кажущееся постоянство в любой момент сменяет непредсказуемость. Если сейчас всё хорошо, значит пришло время.',
 		year: rand(1950..2013),
-		price: rand(1..1000000),
+		price: rand(1..20000),
 		artist: Artist.last,
-		remote_image_url: 'http://lorempixel.com/500/500/'
+		remote_image_url: image
 	).errors.full_messages
 end	
