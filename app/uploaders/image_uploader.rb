d22b5a556318be83a4effff536d70e00bab3ea69
Maxim <drachyov.maxim@gmail.com>
2014-08-29 02:22:47 +0400

    update

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -22,7 +22,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   version :medium do
-    process :resize_to_limit => [470, 380]
+    process :resize_to_limit => [480, 870]
   end
 
   version :preview do
